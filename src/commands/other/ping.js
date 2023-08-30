58212c91a31609109368639665a625cbf572aa3b
robo311 <rhornik311@gmail.com>
2023-08-30 14:02:26 +0200

    Updated ping command

--- src/commands/other/ping.js
+++ src/commands/other/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.reply(`🏓 Latency is ${interaction.createdTimestamp - Date.now() } ms`);
 	},
 };
